dd6af22deb5f172d43a1ac6cc5d2c8c6b2dc82c3
Raik <mail@raik-mueller.com>
2022-08-26 12:52:53 +0200

    feat: webserver with availabe data

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
+#include <WebServer.h>
 
-void setup() {
-  
+
+WebServer server(80);
+
+void handle_Get();
+void handle_NotFound();
+
+
+
+void setup()
+{
+
   Serial.begin(921600);
   Serial.println("Starting setup");
   delay(10);
@@ -10,19 +20,17 @@ void setup() {
   WiFiManager wifiManager;
 
 
-  // If you've previously connected to your WiFi with this ESP32,
-  // WiFi manager will more than likely not do anything.
-  // Uncomment this if you want to force it to delete your old WiFi details.
-  //wifiManager.resetSettings();
-
-  //Tries to connect to last known WiFi details
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
+  // Tries to connect to last known WiFi details
+  // if it does not connect it starts an access point with the specified name
+  // here  "AutoConnectAP"
+  // and goes into a blocking loop awaiting configuration
   if (!wifiManager.autoConnect("WiFiManagerAP", "password")) {
     Serial.println("failed to connect and hit timeout");
-    //reset and try again, or maybe put it to deep sleep
+  
+    // TODO reset and try again, or maybe put it to deep sleep
+
     ESP.restart();
+
     delay(1000);
   }
 
@@ -31,9 +39,33 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
+  // defined paths
+  server.on("/get_data", handle_Get);
+  server.onNotFound(handle_NotFound);
 
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  
+  server.handleClient();
+}
+
+// return fake data
+void handle_Get() {
+  String stranka = "{\n";
+  stranka += F("\"Hysteresis\":");
+  stranka += "100";
+  stranka += F(",\n");
+  stranka += F("\"Target_Temperature\":");
+  stranka += "100";
+  stranka += F(",\n");
+  stranka += F("\"Actual_Temperature\":");
+  stranka += "100";
+  stranka += F("}\n");
+  server.send(200, "application/json", stranka);
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
 }
