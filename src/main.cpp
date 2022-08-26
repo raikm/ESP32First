a60fcdac0389f3a3ecdbae081247501b920bfe1d
Raik <mail@raik-mueller.com>
2022-08-26 10:30:13 +0200

    feat: print message to connected computer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(921600);
+  Serial.println("hello from setup");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("hello from loop");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
 }
